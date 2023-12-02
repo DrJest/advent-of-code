5e99951a1a76dfbfed17b7a62d2cfd920512d7c8
Simone Albano <s.albano@sinapsys.it>
2023-12-02 08:58:29 +0100

    aox 2023

--- 2023/ex/01.js
+++ 2023/ex/01.js
@@ -1,4 +1,61 @@
 const fs = require('fs');
 const path = require('path');
 const input = fs.readFileSync(path.resolve(__dirname, '..', 'inputs', '01.txt'), 'utf8');
-  
+
+const numbersToDigits = {
+  one: '1',
+  '1': '1',
+  two: '2',
+  '2': '2',
+  three: '3',
+  '3': '3',
+  four: '4',
+  '4': '4',
+  five: '5',
+  '5': '5',
+  six: '6',
+  '6': '6',
+  seven: '7',
+  '7': '7',
+  eight: '8',
+  '8': '8',
+  nine: '9',
+  '9': '9',
+};
+
+let result1 = input.split('\n').reduce((acc, curr) => {
+  let numbers = curr.match(/\d/g);
+  let first = numbers.shift();
+  let last = numbers.pop() || first;
+  const result = acc + Number(first * 10) + Number(last);
+  return result;
+}, 0);
+
+console.log(result1);
+
+let result2 = input.split('\n').map((line, index) => {
+  let firstIndex, lastIndex, first, last;
+  for (let k in numbersToDigits) {
+    let p1 = line.indexOf(k);
+    let p2 = line.lastIndexOf(k);
+    if (firstIndex === undefined && p1 !== -1) {
+      firstIndex = p1;
+      first = numbersToDigits[k];
+    }
+    if (lastIndex === undefined && p2 !== -1) {
+      lastIndex = p2;
+      last = numbersToDigits[k];
+    }
+    if (p1 !== -1 && p1 < firstIndex) {
+      firstIndex = p1;
+      first = numbersToDigits[k];
+    }
+    if (p2 !== -1 && p2 > lastIndex) {
+      lastIndex = p2;
+      last = numbersToDigits[k];
+    }
+  }
+  return Number((first ?? '') + (last ?? ''));
+}).reduce((acc, curr) => acc + curr, 0);
+
+console.log(result2);
--- 2023/ex/02.js
+++ 2023/ex/02.js
@@ -1,4 +1,26 @@
 const fs = require('fs');
 const path = require('path');
 const input = fs.readFileSync(path.resolve(__dirname, '..', 'inputs', '02.txt'), 'utf8');
-  
+
+let avCubes = {
+  R: 12,
+  G: 13,
+  B: 14
+};
+
+let [result1, result2] = input.split('\n').map(line => {
+  let game = line.match(/Game (\d+)/)[1];
+  let blue = line.match(/(\d+) blue/g).map(x => parseInt(x.match(/\d+/)[0])).reduce((a, b) => Math.max(a, b), 0);
+  let red = line.match(/(\d+) red/g).map(x => parseInt(x.match(/\d+/)[0])).reduce((a, b) => Math.max(a, b), 0);
+  let green = line.match(/(\d+) green/g).map(x => parseInt(x.match(/\d+/)[0])).reduce((a, b) => Math.max(a, b), 0);
+  return { game, blue, red, green };
+}).reduce((a, b) => {
+  if (b.red <= avCubes.R && b.blue <= avCubes.B && b.green <= avCubes.G) {
+    a[0] += Number(b.game);
+  }
+  a[1] += b.red * b.blue * b.green;
+  return a;
+}, [0, 0]);
+
+console.log(result1);
+console.log(result2);
